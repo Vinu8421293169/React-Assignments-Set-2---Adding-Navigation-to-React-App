4578ccaf31820e6debac89e3c93d80837c3e8147
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-26 16:44:23 +0530

    commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class About extends Component {
   render() {
-    return <>You are on the about page.</>;
+    return <h1>You are on the about page.</h1>;
   }
 }
 
